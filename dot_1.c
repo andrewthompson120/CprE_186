916dd042a976b0c9f9193ceaeea216c6a8ecfc51
andrewthompson120 <andrewthompson120@gmail.com>
2017-01-18 10:39:42 -0600

    Original Files

---
+++ dot_1.c
@@ -0,0 +1,11 @@
+
+//#include <stdio.h>
+
+double dot(double a[], double b[], int length) {
+    int i;
+    double sum;
+    for (i=1; i<= length; i++) {
+        sum = sum + a[i]*b[i];
+    }
+    return sum;
+}
